9fc74df9dff0f8ce9b43b24539b09645f821c255
MdNaeemSheikh <124042468+MdNaeemSheikh@users.noreply.github.com>
2023-02-03 22:35:24 +0600

    MD NAEEM SHEIKH

---
+++ Employee.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args){
+        Box b1=new Box();
+        b1.height=10;
+        b1.weight=4;
+        b1.length=20;
+        b1.calculationArea();
+        Employee e1=new Employee();
+        e1.name="Naeem";
+        e1.salary=200000;
+        e1.display();
+    }
+}
