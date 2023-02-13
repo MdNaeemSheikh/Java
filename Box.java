dc5ae140bd8ee5a5925b10d21cf75fefe494d8b4
MdNaeemSheikh <124042468+MdNaeemSheikh@users.noreply.github.com>
2023-02-13 22:21:06 +0600

    Add files via upload

--- Box.java
+++ Box.java
@@ -1,9 +1,35 @@
 public class Box {
-    int height;
-    int weight;
-    int length;
-    void calculationArea(){
-        int area=this.height|*this.weight*this.length;
-        System.out.println("The area is =" +area);
+    public int height;
+    public int width;
+    public int length;
+
+    public static void Calculate_area(int height, int width, int length) {
+
+        System.out.println(height * width * length);
+    }
+
+    public static void Calculate_area(int height, int width) {
+
+        System.out.println(height * width);
+    }
+
+    public void display() {
+
+        System.out.println(height);
+        System.out.println(width);
+        System.out.println(length);
+    }
+
+    public static void main(String[] args) {
+
+        Calculate_area(3, 7, 7);
+        Calculate_area(4, 20);
+
+        Box obj1 = new Box();
+        obj1.height = 30;
+        obj1.width = 10;
+        obj1.length = 5;
+        obj1.display();
+
     }
 }
